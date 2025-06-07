79143ebf64ab84776651ba830b06e7ce1f9dcec2
Tyler Robertson <tyler.robertson@eventtemple.com>
2025-06-06 21:45:53 -0300

    Update test config

--- jest.config.js
+++ jest.config.js
@@ -10,14 +10,9 @@ module.exports = {
   coveragePathIgnorePatterns: ["node_modules/", "index.ts"],
   coverageReporters: ["text", "lcov", "html", "json-summary"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
   clearMocks: true,
   restoreMocks: true,
   transformIgnorePatterns: ["node_modules/(?!(@octokit|@inquirer)/)"],
   extensionsToTreatAsEsm: [".ts"],
-  moduleNameMapper: {
-    "^@octokit/rest$": "<rootDir>/tests/mocks/github-api.ts",
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
   testTimeout: 15000,
 }
